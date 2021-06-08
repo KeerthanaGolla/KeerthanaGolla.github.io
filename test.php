31cbea8ab99ca35f1581f3574fe069c72cee30fe
Keerthana Golla IBM <83217988+KeerthanaGolla@users.noreply.github.com>
2021-06-08 13:15:54 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,10 @@
 <?php
-foreach ($_POST as $post_var){
-  echo strtoupper($post_var) . "<br />";
-}
+//collect data send as XML 
+$xml = file_get_contents('php://input');
+//open a file handler with read and write permission
+$fh = fopen('userinfo.xml', 'r+');
+//writing XML string to the new file
+fwrite($fh, $xml);
+//closing the file handler
+fclose($fh);
 ?>
