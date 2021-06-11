aad5b2507ba474d204d7c9a01fb11e555e861ca0
Keerthana Golla IBM <83217988+KeerthanaGolla@users.noreply.github.com>
2021-06-11 11:09:29 +0530

    Update newjs.js

--- newjs.js
+++ newjs.js
@@ -151,7 +151,8 @@ xhr.send("name=k&message=yo");*/
 	//var res = XMLHttpRequest.response;
 	//res.setHeader('Access-Control-Allow-Origin', '*');
 	//xmlhttp.setRequestHeader( 'Access-Control-Allow-Origin', '*');
-    xmlhttp.open("POST","http://9.202.179.26:9080/smcfs/restapi/executeFlow/TestDbService",true);
+	alert("http")
+        xmlhttp.open("POST","http://9.202.179.26:9080/smcfs/restapi/executeFlow/TestDbService");
 	//alert("https")
 	//xmlhttp.open("POST","https://9.202.179.26:9443/smcfs/restapi/executeFlow/TestDbService");
 	//xmlhttp.open("POST","https://hookb.in/BYd120wneGILDDx3Bbyw");
