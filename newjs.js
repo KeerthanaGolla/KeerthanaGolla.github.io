db48cd9889506e853bd00c803803957625518878
Keerthana Golla IBM <83217988+KeerthanaGolla@users.noreply.github.com>
2021-06-14 11:11:23 +0530

    Update newjs.js

--- newjs.js
+++ newjs.js
@@ -152,10 +152,10 @@ xhr.send("name=k&message=yo");*/
 	//var res = XMLHttpRequest.response;
 	//res.setHeader('Access-Control-Allow-Origin', '*');
 	//xmlhttp.setRequestHeader( 'Access-Control-Allow-Origin', '*');
-	//alert("http")
-       // xmlhttp.open("POST","http://9.202.179.26:9080/smcfs/restapi/executeFlow/TestDbService");
-	alert("https")
-	xmlhttp.open("POST","https://9.202.179.26:9443/smcfs/restapi/executeFlow/TestDbService");
+	alert("http")
+        xmlhttp.open("POST","http://9.202.179.26:9080/smcfs/restapi/executeFlow/TestDbService");
+	//alert("https")
+	//xmlhttp.open("POST","https://9.202.179.26:9443/smcfs/restapi/executeFlow/TestDbService");
 	//xmlhttp.open("POST","https://hookb.in/BYd120wneGILDDx3Bbyw");
 	var xmlDoc;
 	xmlhttp.onreadystatechange = function() {
